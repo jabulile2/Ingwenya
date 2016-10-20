08a9fd8a2e0d012c15a92e7fa11d48e3f348b0f1
Clementine <toto.clementine@gmail.com>
2016-10-20 12:11:52 +0200

    js yengwenya

---
+++ Ingwenya.js
@@ -0,0 +1,12 @@
+ <script type="text/javascript">
+
+       function myFunction(){
+       	 alert("your name is vincent!");
+
+       }
+        function myFunction(18){
+         var alertstring="your age is" + age + "years old"
+           alert(alertstring);
+        }
+        </script>
+         </form>
